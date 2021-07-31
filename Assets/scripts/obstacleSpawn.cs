8c37d3f3e4e13d621888d67b066474aa7d42f8b4
Termi <escarpaprieto@gmail.com>
2021-07-31 11:50:24 +0200

    Obstacle spawn

--- Assets/scripts/obstacleSpawn.cs
+++ Assets/scripts/obstacleSpawn.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class obstacleSpawn : MonoBehaviour
 {
+    public float startDelay = 1.0f;
+    public float repeatDelay = 3.0f;
+    public GameObject[] obstacleArray;
+     
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("obstacleGenerator",startDelay,repeatDelay);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void obstacleGenerator()
+    {
+        Vector3 spawnPos = new Vector3(40, 0, 0);
+        Instantiate (obstacleArray[0], spawnPos, obstacleArray[0].transform.rotation);
     }
 }
